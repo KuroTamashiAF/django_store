e096e454c0172a428869e106de330d9bd531d8d3
Fedorov Alexandr <chughun777akk@mail.ru>
2023-10-30 20:16:03 +0700

    create Model Product

--- StoreProject/Store_App/models.py
+++ StoreProject/Store_App/models.py
@@ -11,3 +11,15 @@ class Client(models.Model):
     def __str__(self):
         return (f"{self.name_client} {self.number_phone_client} {self.email_client} {self.address_client}"
                 f"{self.date_registrations_client}")
+
+
+class Product(models.Model):
+    name_product = models.CharField(max_length=100)
+    description_product = models.TextField()
+    price_product = models.DecimalField(max_digits=7, decimal_places=2)
+    quantity_product = models.IntegerField()
+    date_registrations_product = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return (f"{self.name_product} {self.price_product} {self.date_registrations_product} "
+                f"{self.quantity_product}")
