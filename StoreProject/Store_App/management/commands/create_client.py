814d72d619e7d35b7f6b1638749ea8652b4b027e
Fedorov Alexandr <chughun777akk@mail.ru>
2023-10-31 07:46:48 +0700

    command Create Client is work

---
+++ StoreProject/Store_App/management/commands/create_client.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from Store_App.models import Client
+
+
+class Command(BaseCommand):
+    help = "create Client and write him in DB"
+
+    def handle(self, *args, **options):
+        client = Client(name_client="Alex", email_client="sofa@mouse.net",
+                        number_phone_client="+39264564563",
+                        address_client="island Paradise from AoT")
+        client.save()
+
