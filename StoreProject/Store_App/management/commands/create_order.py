e8db99f2611e35de879c3f5dfe6365e894897c58
Fedorov Alexandr <chughun777akk@mail.ru>
2023-11-16 18:07:48 +0700

    create command 'create order'

--- StoreProject/Store_App/management/commands/create_order.py
+++ StoreProject/Store_App/management/commands/create_order.py
@@ -5,18 +5,20 @@ from Store_App.models import Product, Order, Client
 class Command(BaseCommand):
     help = "create order"
 
+    def add_arguments(self, parser):
+        parser.add_argument("order_client", type=int, help="id client")
+        parser.add_argument("order_product", type=int, help="id product")
+
     def handle(self, *args, **options):
-        client = Client(name_client="Golum",
-                        email_client="misty mountains@Mordor.do",
-                        number_phone_client="+668",
-                        address_client="misty mountains, cave")
-        client.save()
-        product = Product(name_product="The Ring of Omnipotence",
-                          description_product="One Ring rule all them ",
-                          price_product=10000.896,
-                          quantity_product=1)
-        product.save()
+        id_client = options.get("order_client")
+        client = Client.objects.get(pk=id_client)
+
+        id_product = options.get("order_product")
+        product = Product.objects.get(pk=id_product)
+
+        total_price = product.price_product
+
         order = Order(order_client=client,
                       order_product=product,
-                      total_price=product.price_product)
+                      total_price=total_price)
         order.save()
