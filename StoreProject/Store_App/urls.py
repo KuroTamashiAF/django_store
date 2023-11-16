1bff1e5f50b0c4046b1b8efd195b9bc58ffa7169
Fedorov Alexandr <chughun777akk@mail.ru>
2023-11-16 19:02:27 +0700

    create view 'all_products'

--- StoreProject/Store_App/urls.py
+++ StoreProject/Store_App/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import out_order
 
 urlpatterns = [
-    path('order/<int:client_id>/', out_order, name="out_order"),
+    path('client/<int:client_id>/', out_order, name="out_order"),
 
 ]
--- StoreProject/Store_App/views.py
+++ StoreProject/Store_App/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
 from .models import Order, Client, Product
+from datetime import date, timedelta
 
 
 def out_order(request, client_id):
@@ -14,3 +15,13 @@ def out_order(request, client_id):
     return render(request, "Store_App/order_out.html", {"client": client, "orders": orders,
                                                         "products": products,
                                                         "total_price_all": total_price_all})
+
+
+def all_products(request, client_id, num_days_ago):
+    client = Client.objects.get(pk=client_id)
+    now = date.today()
+    before = now - timedelta(days=num_days_ago)
+    orders = Order.objects.filter(order_client=client, date_order__range=(before, now))
+    return render(request, "", {"before": before,
+                                "orders": orders,
+                                "client": client})
