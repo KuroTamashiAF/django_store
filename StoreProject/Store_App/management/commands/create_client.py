ffb8afb3dd7497f31ec48c38186254262dc36cd6
Fedorov Alexandr <chughun777akk@mail.ru>
2023-11-16 17:44:30 +0700

    create command 'create client' and 'create product'

--- StoreProject/Store_App/management/commands/create_client.py
+++ StoreProject/Store_App/management/commands/create_client.py
@@ -5,9 +5,26 @@ from Store_App.models import Client
 class Command(BaseCommand):
     help = "create Client and write him in DB"
 
-    def handle(self, *args, **options):
-        client = Client(name_client="Alex", email_client="sofa@mouse.net",
-                        number_phone_client="+39264564563",
-                        address_client="island,Paradise,from,AoT")
-        client.save()
+    def add_arguments(self, parser):
+        parser.add_argument("name", type=str, help="name client")
+        parser.add_argument("email", type=str, help="email client")
+        parser.add_argument("number_phone", type=str, help="number_phone client")
+        parser.add_argument("address_client", type=str, help="address_client client")
 
+    def handle(self, *args, **options):
+        name_ = options["name"]
+        name = name_.replace("_", " ")
+        email_ = options.get("email")
+        email = email_.replace("_", " ")
+        number_phone_ = options.get("number_phone")
+        number_phone = number_phone_.replace("_", " ")
+        address_client_ = options.get("address_client")
+        address_client = address_client_.replace("_", " ")
+        address_client_ = address_client.replace("_", " ")
+        address_client = address_client_.replace("_", " ")
+
+        client = Client(name_client=name,
+                        email_client=email,
+                        number_phone_client=number_phone,
+                        address_client=address_client)
+        client.save()
--- StoreProject/Store_App/management/commands/create_product.py
+++ StoreProject/Store_App/management/commands/create_product.py
@@ -2,13 +2,25 @@ from django.core.management.base import BaseCommand
 from Store_App.models import Product
 
 
+
 class Command(BaseCommand):
     help = "crete the product"
 
+    def add_arguments(self, parser):
+        parser.add_argument("name_product", type=str)
+        parser.add_argument("description_product", type=str)
+        parser.add_argument("price_product", type=float)
+        parser.add_argument("quantity_product", type=int)
+
     def handle(self, *args, **options):
-        product = Product(name_product="Microwave Eaton 7500",
-                          description_product="Microwave for food",
-                          price_product=15489.459,
-                          quantity_product=10)
+        name_product = options["name_product"].replace("_", " ")
+        description_product = options["description_product"].replace("_", " ")
+        price_product = options["price_product"]
+        quantity_product = options["quantity_product"]
+
+        product = Product(name_product=name_product,
+                          description_product=description_product,
+                          price_product=price_product,
+                          quantity_product=quantity_product)
         product.save()
         self.stdout.write(f"{product}")
