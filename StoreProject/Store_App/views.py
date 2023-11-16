09882793aaa337605b21fc051f7f0a3ee8d570ae
Fedorov Alexandr <chughun777akk@mail.ru>
2023-11-16 07:48:49 +0700

    create template order

--- StoreProject/Store_App/views.py
+++ StoreProject/Store_App/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Order, Client, Product
 
-# Create your views here.
+
+def out_order(request, client_id):
+    client = Client.objects.get(id=client_id)
+    order = Order.objects.filter(order_client=client)
+    render(request, "", {"client": client, "order": order})
